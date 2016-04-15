43e8645c3fb1b7056c97096ed7339e191f4d32aa
Beorn Silemmo <silemmo@gmail.com>
2016-04-16 01:45:43 +0200

    initial commit

---
+++ elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (22285 22810 50376 506000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
